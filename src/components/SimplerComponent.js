556fe90e2b8e4fcadcd344a601e62de04042042e
Sura Al-jilawi <suraaljailawi@gmail.com>
2020-01-21 14:15:44 +0000

    Automatically backed up by Learn

--- src/components/SimplerComponent.js
+++ src/components/SimplerComponent.js
@@ -1,1 +1,32 @@
 // Code SimplerComponent Here
+import React, { Component } from 'react';
+class SimplerComponent extends Component {
+    state = {
+    moode: happy
+      }
+      handleClick () => {
+
+     switch (this.state.mood) {
+       case "happy":this.setState({
+         mood:"sad"
+       })
+
+         break;
+         case "sad":this.setState({
+           mood:"happy"
+         })
+
+           break;
+
+
+     }
+
+      }
+    render() {
+        return (
+  <div onClick={this.handleClick }> {this.state.mode} </div>
+         );
+    }
+}
+
+export default SimplerComponent;
