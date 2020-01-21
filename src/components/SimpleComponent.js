ecd3c22256f78e06232b0aad1b133ba6a6671fa4
Sura Al-jilawi <suraaljailawi@gmail.com>
2020-01-21 14:14:26 +0000

    Automatically backed up by Learn

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,32 @@
 // Code SimpleComponent Here
+import React, { Component } from 'react';
+class SimpleComponent extends Component {
+    state = {
+    moode: happy
+      }
+      handleClick () => {
+
+     switch (this.state.mood) {
+       case "happy":this.setState({
+         mood:"sad"
+       })
+         
+         break;
+         case "sad":this.setState({
+           mood:"happy"
+         })
+           
+           break;
+       
+         
+     }
+        
+      }
+    render() { 
+        return ( 
+  <div onClick={this.handleClick }> {this.state.mode} </div>
+         );
+    }
+}
+ 
+export default SimpleComponent;
